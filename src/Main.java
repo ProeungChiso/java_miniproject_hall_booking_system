c6f309b1b4fe989ce846b2e931fb7d560e2c70e1
Proeung Chiso <122223308+ProeungChiso@users.noreply.github.com>
2024-01-26 21:26:23 +0700

    greeting msg

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void welcomeDisplay(){
+        System.out.println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        System.out.println("CSTAD HALL BOOKING SYSTEM");
+        System.out.println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+    }
     public static void main(String[] args) {
+        //Greeting to user
+        welcomeDisplay();
     }
 }
